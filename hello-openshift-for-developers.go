53df15c81805ae877918568ec1cb8a7fa60c0b60
bachld <bachld82@gmail.com>
2023-01-06 00:08:11 +0700

    third test

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World!"
+		response = "Hello Openshif for Developers!"
 	}
 
 	fmt.Fprintln(w, response)
